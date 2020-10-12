90e826316e78170153a22f558f3468cd51f679e3
Susan GURUNG <200020782@stu.vtc.edu.hk>
2020-10-12 12:15:39 +0800

    Test 3

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -39,7 +39,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
         Me.ClientSize = New System.Drawing.Size(800, 450)
         Me.Controls.Add(Me.Label1)
         Me.Name = "Form1"
